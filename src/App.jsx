60b657846d9216b66c2d948f7e09848d7aa57b8c
hasanulhasan <hasanlink71@yahoo.com>
2024-01-22 12:07:59 +0600

    html converted to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Tasks from "./components/Tasks";
 
 function App() {
 
   return (
-    <>
-      <h1>Tasker Project initiated</h1>
-    </>
+    <div className="bg-[#191D26] font-[Inter] text-white">
+      <Navbar/>
+      <Hero/>
+      <Tasks/>
+      <Footer/>
+    </div>
   )
 }
 
